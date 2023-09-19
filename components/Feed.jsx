f1f8752e6747c4aa1762a0fe8ced7a5917fc218d
Anthony Ham <mail@anthonymham.com>
2023-09-19 19:14:47 -0400

    user profile with their posts displaying

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -25,15 +25,15 @@ const Feed = () => {
 
   }
 
-  const fetchPosts = async () => {
-    const response = await fetch('/api/prompt')
-    const data = await response.json()
-    setPosts(data) 
-  }
-
   useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt')
+      const data = await response.json();
+      setPosts(data)
+    }
+
     fetchPosts();
-  })
+  }, [])
 
 
   return (
--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -7,8 +7,11 @@ import { usePathname, useRouter } from "next/navigation";
 
 const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
   const [copied, setCopied] = useState('')
-
-
+  const handleCopy = () => {
+    setCopied(post.prompt)
+    navigator.clipboard.writeText(post.prompt)
+    setTimeout(() => setCopied(""), 3000)
+  }
 
   return (
     <div className="prompt_card">
@@ -20,7 +23,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
         </div>
         </div>
 
-        <div className="copy_btn" onClick={() => {}}>
+        <div className="copy_btn" onClick={handleCopy}>
           <Image src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'} width={12} height={12}/>
         </div>
       </div>
