e27c082695c39ab88560407bbdf5f37bd605021c
Anthony Ham <mail@anthonymham.com>
2023-09-19 20:57:23 -0400

    posts are now editable

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -12,6 +12,7 @@ export const GET = async (req, { params }) => {
     return new Response(JSON.stringify(prompts), {status: 200})
     
   } catch (error) {
+    console.log(error)
     return new Response("Failed to fetch all prompts", {status: 500})
   }
 }
